8c56a33942c2daac91cf97d15761542bab254cd6
Luccas Trindade Guimaraes <luccastrindadeguimaraes@gmail.com>
2024-06-25 05:47:49 -0300

    Caching

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -9,13 +9,18 @@ import br.com.forum.repository.CursoRepository;
 import br.com.forum.repository.TopicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/topicos")
@@ -27,12 +32,15 @@ public class TopicosController {
    private CursoRepository cursoRepository;
 
    @GetMapping
-   public List<TopicoDto> lista(String nomeCurso) {
+   @Cacheable(value = "listaDeTopicos")
+   public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+                                @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 5) Pageable paginacao) {
+
       if (nomeCurso==null) {
-         List<Topico> topicos = topicoRepository.findAll();
+         Page<Topico> topicos = topicoRepository.findAll(paginacao);
          return TopicoDto.converter(topicos);
       } else {
-         List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+         Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
          return TopicoDto.converter(topicos);
       }
    }
--- src/main/java/br/com/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/forum/repository/TopicoRepository.java
@@ -1,11 +1,11 @@
 package br.com.forum.repository;
 
 import br.com.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-   List<Topico> findByCursoNome(String nomeCurso);
+   Page<Topico> findByCursoNome(String nomeCurso, Pageable pageable);
 }
